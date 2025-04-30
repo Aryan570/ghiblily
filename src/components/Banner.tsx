a25ebadb91590078e0d090cf090c5de97c314103
Aryan <malhotraaryan570@gmail.com>
2025-04-30 16:33:43 +0530

    banner_complete

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,9 +4,17 @@ import banner from '@/../public/banner.jpg'
 
 const Banner = ({ tog_ban }: { tog_ban: () => void }) => {
   return (
-    <div className='h-screen w-full overflow-hidden'>
-      <Image className='object-cover w-full h-full' placeholder='blur' src={banner} width={1000} height={1000} alt='banner_inage'/>
-        <button className='cursor-pointer' onClick={tog_ban}>Enter</button>
+    <div className='h-screen w-full overflow-hidden pompiere-font'>
+      <div className='absolute flex w-full h-full justify-around items-center'>
+        <div className=''>
+          <h1 className='text-5xl font-bold text-center hero_portfolio rounded-xl mb-2 p-2 -skew-2'>Welcome to my Ghiblily</h1>
+          <button className='cursor-pointer hero_portfolio rounded-xl w-full text-2xl border-0 -skew-2' onClick={tog_ban}>Enter</button>
+        </div>
+        <div className='p-4 rounded-xl hero_portfolio w-fit m-2'>
+          <p className='text-2xl text-wrap wrap-break-word relative max-w-sm'>Whenever someone creates something with all of their heart, then that creation is given a soul.<br></br> <span className='text-nowrap float-right'>- Hayao Miyazaki</span></p>
+        </div>
+      </div>
+      <Image className='object-cover w-full h-full' placeholder='blur' src={banner} width={1000} height={1000} alt='banner_inage' />
     </div>
   )
 }
