913ebbf64f0bbb53d1edc78aa678d70221af7662
Aryan <malhotraaryan570@gmail.com>
2025-04-30 11:28:37 +0530

    banner&theme

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -18,17 +18,17 @@ const Blogs = async () => {
     const blogs: Blog[] = await blogs_metadata.json();
     return (
         <div className='min-h-screen hero w-full flex flex-col justify-center items-center pompiere-font'>
-            <div className='font-bold text-2xl mb-4'>Welcome to the rock</div>
+            <div className='font-bold w-1/2 text-2xl mb-2 left-0'>Welcome to the rock</div>
             {blogs.map((blog) => (
-                <Link className='w-1/2 hover:scale-[1.03] transition-transform duration-300 ease-out mb-2.5 backdrop-blur-xs' key={blog._id} href={blog.link}>
+                <Link className='md:w-1/2 w-4/5 hover:scale-[1.03] transition-transform duration-300 ease-out mb-2.5 backdrop-blur-xs' key={blog._id} href={blog.link}>
                     <div className='flex border-1 border-slate-400 max-h-1/12 h-[20vh] overflow-hidden'>
-                        <div className='w-3/5 justify-center flex flex-col p-4'>
-                            <div className='text-2xl font-bold overflow-clip underline underline-offset-2'>{blog.title}</div>
-                            <div className=' overflow-clip'>{blog.description}</div>
+                        <div className='w-3/5 justify-start flex flex-col p-4'>
+                            <div className='md:text-2xl font-bold overflow-clip underline underline-offset-2'>{blog.title}</div>
+                            <p className='overflow-clip text-ellipsis'>{blog.description}</p>
                             <div>{new Date(blog.created_at).toLocaleDateString()}</div>
                         </div>
                         <div className='w-2/5 h-full flex justify-center items-center'>
-                            <Image className='object-cover w-full mask-radial-[95%_100%] mask-radial-from-40% mask-radial-at-right contrast-75' src={blog.banner_url} height={100} width={100} alt={blog.title} />
+                            <Image className='object-cover w-full h-full mask-radial-[95%_100%] mask-radial-from-30% mask-radial-at-right contrast-75' src={blog.banner_url} height={200} width={200} alt={blog.title} />
                         </div>
                     </div>
                 </Link>
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,8 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
+import banner from '@/../public/banner.jpg'
 
 const Banner = ({ tog_ban }: { tog_ban: () => void }) => {
   return (
-    <div>
+    <div className='h-screen w-full overflow-hidden'>
+      <Image className='object-cover w-full h-full' placeholder='blur' src={banner} width={1000} height={1000} alt='banner_inage'/>
         <button className='cursor-pointer' onClick={tog_ban}>Enter</button>
     </div>
   )
