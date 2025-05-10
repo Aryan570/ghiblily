3e310b395e06a8a58c66a197ca42e528d4c024a7
Aryan <malhotraaryan570@gmail.com>
2025-05-10 16:56:43 +0530

    smooooooth_again

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -5,13 +5,13 @@ import { useEffect } from 'react'
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     const lenis = new Lenis({
-    //   duration: 1.2,
-    //   easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-    //   orientation: 'vertical',
-    //   gestureOrientation: 'vertical',
-    //   smoothWheel: true,
-    //   syncTouch: false,
-    //   touchMultiplier: 2,
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      orientation: 'vertical',
+      gestureOrientation: 'vertical',
+      smoothWheel: true,
+      syncTouch: false,
+      touchMultiplier: 2,
     })
 
     function raf(time: number) {
