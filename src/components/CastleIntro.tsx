8db56a9b21d588e58a5a445ba723cf64bc9b2a6c
Aryan <malhotraaryan570@gmail.com>
2025-08-20 11:18:18 +0530

    some_design_issues_spiderman

--- src/components/CastleIntro.tsx
+++ src/components/CastleIntro.tsx
@@ -37,23 +37,23 @@ const Intro = () => {
 
   return (
     <div
-      className='relative h-[100dvh] md:h-[60dvh] bg-gray-900 text-white flex items-center justify-center overflow-hidden'
+      className='relative h-[105dvh] md:h-[60dvh] bg-gray-900 text-white flex items-center justify-center overflow-hidden'
       ref={sectionRef}
     >
       <div className="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent"></div>
       <div className="relative z-10 w-full max-w-6xl mx-auto p-6 flex flex-col md:flex-row items-center justify-center gap-8 md:gap-16">
         <div
           ref={boxRef}
-          className="w-full md:w-[65%] h-64 md:h-[70%] shadow-xl flex items-center justify-center p-4 text-white text-3xl font-bold flex-shrink-0 transform-gpu "
+          className="w-[80%] md:w-[65%] h-64 md:h-[70%] shadow-xl flex items-center justify-center p-4 text-white text-3xl font-bold flex-shrink-0 transform-gpu "
         >
           <Image src={castle} width={300} height={300} alt='castle-from-howl' />
         </div>
-        <div className="w-full md:w-[35%] text-center md:text-left p-4 bg-white/10 backdrop-blur-sm rounded-xl shadow-lg text-white flex-shrink-0">
+        <div className="w-full md:w-[45%] text-center md:text-left p-4 bg-white/10 backdrop-blur-sm rounded-xl shadow-lg text-white flex-shrink-0">
           {/* <h2 className="text-3xl font-bold mb-4">Unique Section Title</h2> */}
-          <p className="text-sm md:text-lg leading-relaxed opacity-80 text-left">
+          <p className=" border-l-4 text-lg md:text-xl border-rose-600 bg-rose-400/50 text-left pl-4 italic py-4 mb-1 rounded-r-lg">
             My name is Aryan. I was bitten by a radioactive spider, and for ten years I&apos;ve been the one and only Spider-Man. I&apos;m pretty sure you know the rest. I saved a bunch of people, fell in love, saved the city, and then I saved the city again... and again and again and again. And I, uh...
           </p>
-          <p className="text-sm md:text-lg leading-relaxed text-left">
+          <p className="text-lg md:text-xl leading-relaxed text-left">
             Well, I&apos;m <span className='text-rose-500 font-bold'>Aryan</span> from Mukerian, Punjab, India. I graduated from <span className='decoration-wavy underline decoration-1 underline-offset-2 text-rose-500 font-bold'>NIT Jalandhar</span> with a degree in Electrical Engineering in 2024. I fell in love with <span className='decoration-wavy underline decoration-1 underline-offset-2 text-rose-500 font-bold'>programming</span> during my college years and have been <span className='decoration-wavy underline decoration-1 underline-offset-2 text-rose-500 font-bold'>programming</span> ever since. Besides all that, I love <span className='decoration-wavy underline decoration-1 underline-offset-2 text-rose-500 font-bold'>football</span> and enjoy watching animated movies, mainly from <span className='decoration-wavy underline decoration-1 underline-offset-2 text-rose-500 font-bold'>Pixar</span> and <span className='decoration-wavy underline decoration-1 underline-offset-2 text-rose-500 font-bold'>Studio Ghibli</span>. I could go on talking about the things I love, but I have my blog for that, so check it out.
           </p>
         </div>
